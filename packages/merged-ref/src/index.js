785a7c3bc946cc4619558ee36ea19800b0f58dc6
Jared Lunde <jared.lunde@gmail.com>
2019-09-17 23:35:14 -0600

    adds merged-ref hook

---
+++ packages/merged-ref/src/index.js
@@ -0,0 +1,22 @@
+import {useCallback} from 'react'
+
+const setRef = (ref, value) => {
+  if (typeof ref === 'function') {
+    ref(value)
+  } else if (ref !== null) {
+    ref.current = value
+  }
+}
+
+const useMergedRef = (...args) => {
+  return useCallback(element => {
+    if (args.length === 2) {
+      setRef(args[0], element)
+      setRef(args[1], element)
+    } else {
+      for (let ref of args) setRef(ref, element)
+    }
+  }, args)
+}
+
+export default useMergedRef
