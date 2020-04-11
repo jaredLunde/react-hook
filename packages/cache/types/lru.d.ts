4f435c603ba9cc08c028bc3972ca501c559bcb47
Jared Lunde <jared.lunde@gmail.com>
2020-04-11 01:08:41 -0600

    [cache] alpha bump

--- packages/cache/types/lru.d.ts
+++ packages/cache/types/lru.d.ts
@@ -4,12 +4,12 @@ export declare const lru: <Key = string, Value = any>(
 export declare type LRUCache<Key = string, Value = any> = {
   head: LRUNode<Key, Value> | undefined
   size: number
-  insertHead(node: LRUNode<Key, Value>): void
+  _insertHead(node: LRUNode<Key, Value>): void
+  _deleteNode(node: LRUNode<Key, Value>): void
   forEach(fn: (key: Key, value: Value) => void): void
   search(key: Key): LRUNode<Key, Value>
   read(key: Key): Value
   write(key: Key, value: Value): Value
-  deleteNode(node: LRUNode<Key, Value>): void
   delete(key: Key): Value
   pop(): Value | undefined
 }
