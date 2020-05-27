1782ca9758e554c42fd2c98a74de266e6a8b155b
Jared Lunde <jared.lunde@gmail.com>
2020-05-27 08:44:24 -0600

    [window-scroll] fixes window is undefined error in SSR

--- packages/window-scroll/src/index.ts
+++ packages/window-scroll/src/index.ts
@@ -8,13 +8,15 @@ const getScrollY = (): number =>
     ? 0
     : window.pageYOffset
 
+const win = typeof window === 'undefined' ? null : window
+
 export const useWindowScroll = (fps = 30): number => {
   const [scrollY, setThrottledScroll] = useThrottle(
     typeof window === 'undefined' ? 0 : getScrollY,
     fps,
     true
   )
-  useEvent(window, 'scroll', (): void => setThrottledScroll(getScrollY()))
+  useEvent(win, 'scroll', (): void => setThrottledScroll(getScrollY()))
   return scrollY
 }
 
