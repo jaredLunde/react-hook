35d1b54243ea90eac3ae079bff7034f34d784e08
Jared Lunde <jared.lunde@gmail.com>
2020-06-07 10:15:55 -0600

    [mouse-position] Updates build

--- packages/mouse-position/src/index.test.tsx
+++ packages/mouse-position/src/index.test.tsx
@@ -1,6 +1,6 @@
-import {fireEvent} from '@testing-library/dom'
-import {renderHook, act} from '@testing-library/react-hooks'
-import useMouse from './index'
+// import {fireEvent} from '@testing-library/dom'
+// import {renderHook, act} from '@testing-library/react-hooks'
+// import useMouse from './index'
 
 it('passes', () => {
   expect(true).toBe(true)
