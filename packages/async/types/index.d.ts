8a9ad91308bbc21f73df14353c421b5e7ae5be22
Jared Lunde <jared.lunde@gmail.com>
2020-07-06 13:15:40 -0600

    build(all): upgrade lundle

--- packages/async/types/index.d.ts
+++ packages/async/types/index.d.ts
@@ -1,46 +1,25 @@
-import * as React from 'react'
-export declare const useAsync: <
-  ValueType extends unknown = any,
-  ErrorType extends unknown = Error,
-  Args extends any[] = any[]
->(
-  asyncCallback: (...args: Args) => Promise<ValueType>
-) => [AsyncState<ValueType, ErrorType, Args>, AsyncCallback<Args>]
-export declare const useAsyncEffect: <
-  ValueType extends unknown = any,
-  ErrorType extends unknown = Error
->(
-  asyncCallback: () => Promise<ValueType>,
-  dependencies?: React.DependencyList | undefined
-) => AsyncState<ValueType, ErrorType, []>
+import * as React from 'react';
+export declare const useAsync: <ValueType extends unknown = any, ErrorType extends unknown = Error, Args extends any[] = any[]>(asyncCallback: (...args: Args) => Promise<ValueType>) => [AsyncState<ValueType, ErrorType, Args>, AsyncCallback<Args>];
+export declare const useAsyncEffect: <ValueType extends unknown = any, ErrorType extends unknown = Error>(asyncCallback: () => Promise<ValueType>, dependencies?: React.DependencyList | undefined) => AsyncState<ValueType, ErrorType, []>;
 export interface AsyncReducerState<ValueType, ErrorType> {
-  status: AsyncStatus
-  value?: ValueType
-  error?: ErrorType
+    status: AsyncStatus;
+    value?: ValueType;
+    error?: ErrorType;
 }
-export interface AsyncState<ValueType, ErrorType, Args extends any[] = any[]>
-  extends AsyncReducerState<ValueType, ErrorType> {
-  cancel: () => void
+export interface AsyncState<ValueType, ErrorType, Args extends any[] = any[]> extends AsyncReducerState<ValueType, ErrorType> {
+    cancel: () => void;
 }
-export declare type AsyncAction<ValueType, ErrorType> =
-  | {
-      status: 'idle' | 'loading' | 'cancelled'
-    }
-  | {
-      status: 'success'
-      value: ValueType
-    }
-  | {
-      status: 'error'
-      error?: ErrorType
-    }
+export declare type AsyncAction<ValueType, ErrorType> = {
+    status: 'idle' | 'loading' | 'cancelled';
+} | {
+    status: 'success';
+    value: ValueType;
+} | {
+    status: 'error';
+    error?: ErrorType;
+};
 export interface AsyncCallback<Args extends any[] = any[]> {
-  (...args: Args): void
-  cancel: () => void
+    (...args: Args): void;
+    cancel: () => void;
 }
-export declare type AsyncStatus =
-  | 'idle'
-  | 'loading'
-  | 'success'
-  | 'error'
-  | 'cancelled'
+export declare type AsyncStatus = 'idle' | 'loading' | 'success' | 'error' | 'cancelled';
