d42125815b7decaa3dc229158b7d40e16928e644
Jared Lunde <jared@getadmiral.com>
2020-07-22 08:35:09 -0600

    chore(release): window-size 3.0.7

--- packages/window-size/throttled/types/index.d.ts
+++ packages/window-size/throttled/types/index.d.ts
@@ -1,9 +1,19 @@
 export interface ThrottledWindowSizeOptions {
-    initialWidth?: number;
-    initialHeight?: number;
-    fps?: number;
-    leading?: boolean;
+  initialWidth?: number
+  initialHeight?: number
+  fps?: number
+  leading?: boolean
 }
-export declare const useWindowSize: (options?: ThrottledWindowSizeOptions) => readonly [number, number];
-export declare const useWindowHeight: (options?: Pick<ThrottledWindowSizeOptions, "initialHeight" | "fps" | "leading"> | undefined) => number;
-export declare const useWindowWidth: (options?: Pick<ThrottledWindowSizeOptions, "initialWidth" | "fps" | "leading"> | undefined) => number;
+export declare const useWindowSize: (
+  options?: ThrottledWindowSizeOptions
+) => readonly [number, number]
+export declare const useWindowHeight: (
+  options?:
+    | Pick<ThrottledWindowSizeOptions, 'initialHeight' | 'fps' | 'leading'>
+    | undefined
+) => number
+export declare const useWindowWidth: (
+  options?:
+    | Pick<ThrottledWindowSizeOptions, 'initialWidth' | 'fps' | 'leading'>
+    | undefined
+) => number
