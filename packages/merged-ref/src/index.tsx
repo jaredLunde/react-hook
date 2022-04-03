959d5354d6fd370f3b2a453a33ae17c66d8a3fea
Vigen <47vigen@gmail.com>
2022-04-03 12:41:56 +0430

    fix null or undefined ref

--- packages/merged-ref/src/index.tsx
+++ packages/merged-ref/src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 
-function useMergedRef<T>(...refs: React.Ref<T>[]): React.RefCallback<T> {
+function useMergedRef<T>(...refs: (React.Ref<T> | undefined | null)[]): React.RefCallback<T> {
   return React.useCallback(
     (element: T) => {
-      for (let i = 0; i < refs.length; i++) {
-        const ref = refs[i]
+      const _refs = refs.filter(Boolean)
+      for (let i = 0; i < _refs.length; i++) {
+        const ref = _refs[i]
         if (typeof ref === 'function') ref(element)
         else if (ref && typeof ref === 'object')
           (ref as React.MutableRefObject<T>).current = element
